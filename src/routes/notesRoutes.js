a85c1d02b7981a834a08d34940ce3c34c125079d
mad-jules <yuliia.pechera@gmail.com>
2025-10-16 11:43:08 +0300

    feat: define routes for fetching all notes and note by ID

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,13 +1,7 @@
-export const getAllNotes = (req, res) => {
-  console.log('test');
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-};
+import { Router } from 'express';
+import { getAllNotes, getNoteById } from '../controllers/notesController.js';
 
-export const getNoteById = (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-};
+export const router = Router();
+
+router.get('/notes', getAllNotes);
+router.get('/notes/:noteId', getNoteById);
