a9cac59ff7c360ca0fd16d3d587aa7b06874694f
mad-jules <yuliia.pechera@gmail.com>
2025-10-16 13:50:19 +0300

    feat: add HTTP error handling with http-errors, update notesController and errorHandler

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,16 +1,18 @@
 import { Note } from '../models/note.js';
+import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res, next) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
 
-export const getNoteById = async (req, res) => {
+export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
 
   if (!note) {
-    return res.status(404).json({ message: 'Note not found' });
+    next(createHttpError(404, 'Student not found'));
+    return;
   }
 
   res.status(200).json(note);
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,19 @@
+import { HttpError } from 'http-errors';
+
 export function errorHandler(error, req, res, next) {
+  console.error('Error Middleware:', error);
+
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      message: error.message || error.name,
+    });
+  }
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: 'Internal Server Error',
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : error.message,
   });
 }
