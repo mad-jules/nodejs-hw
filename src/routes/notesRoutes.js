87a09e2371f8f1412be0f3141daf32c86e3748fd
mad-jules <yuliia.pechera@gmail.com>
2025-10-18 13:28:01 +0300

    feat: implement POST, DELETE and PATCH methods

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
-import { getAllNotes, getNoteById } from '../controllers/notesController.js';
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
+} from '../controllers/notesController.js';
 
 export const router = Router();
 
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
