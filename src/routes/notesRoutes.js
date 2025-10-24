d7cb62350cb69b9551050e7cd468e56b960f33dc
mad-jules <yuliia.pechera@gmail.com>
2025-10-24 16:30:40 +0300

    refactor: add celebrate validation middleware to GET /notes route

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -9,13 +9,14 @@ import {
 import { celebrate } from 'celebrate';
 import {
   createNoteSchema,
+  getAllNotesSchema,
   noteIdSchema,
   updateNoteSchema,
 } from '../validations/notesValidation.js';
 
 export const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
