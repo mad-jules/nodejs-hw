8acc68b68c03a05184b2f175dccf8859f8218d24
mad-jules <yuliia.pechera@gmail.com>
2025-10-24 16:29:44 +0300

    refactor: wrap getAllNotesSchema in Segments.QUERY for request validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -2,14 +2,16 @@ import { Joi, Segments } from 'celebrate';
 import { TAGS } from '../constants/tags.js';
 import { isValidObjectId } from 'mongoose';
 
-export const getAllNotesSchema = Joi.object({
-  page: Joi.number().integer().min(1).default(1),
-  perPage: Joi.number().integer().min(5).max(20).default(10),
-  tag: Joi.string()
-    .valid(...TAGS)
-    .optional(),
-  search: Joi.string().allow('').optional(),
-});
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+    search: Joi.string().allow('').optional(),
+  }),
+};
 
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
