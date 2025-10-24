a1866aa0c15a8a7f3b22715a8a3be4c63327a3f4
mad-jules <yuliia.pechera@gmail.com>
2025-10-24 12:03:27 +0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { router } from './routes/notesRoutes.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
