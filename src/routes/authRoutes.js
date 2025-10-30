b9879fb23f3d868026dd59f0a02768ef698f7f12
mad-jules <yuliia.pechera@gmail.com>
2025-10-30 12:23:10 +0200

    feat(auth): add authentication routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+} from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
+
+export const router = Router();
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
